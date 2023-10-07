d1ae96fcf734daad79d99febb55b78704e50e7e5
ChirkinaAnastasia <chirkina.developer@gmail.com>
2023-10-07 17:53:08 +0300

    create modal.js

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const modalTitleAdd = document.querySelector('form__title-add');
+const modalTitleChange = document.querySelector('form__title-change');
+
+const modalIdNumber = document.querySelector('form__id-number');
+
+const modalClose = document.querySelector('modal__main_close');
+const modalCloseButton = document.querySelector('modal__close-button');
+
+const modalForm = document.querySelector('modal__form');
+const modalFormCheckbox = document.querySelector('form__input-checkbox');
+const modalFormDiscountInput = document.querySelector('form__input-discount');
+
+const modalTotalPriceNumber = document.querySelector('total__sum');
