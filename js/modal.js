cec609a868bc57487f076dc922c5824daaac4b42
ChirkinaAnastasia <chirkina.developer@gmail.com>
2023-10-08 16:06:55 +0300

    add . and console.log

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,24 @@
 'use strict';
 
-const modalTitleAdd = document.querySelector('form__title-add');
-const modalTitleChange = document.querySelector('form__title-change');
+const modalTitleAdd = document.querySelector('.form__title-add');
+console.log(modalTitleAdd);
+const modalTitleChange = document.querySelector('.form__title-change');
+console.log(modalTitleChange);
 
-const modalIdNumber = document.querySelector('form__id-number');
+const modalIdNumber = document.querySelector('.form__id-number');
+console.log(modalIdNumber);
 
-const modalClose = document.querySelector('modal__main_close');
-const modalCloseButton = document.querySelector('modal__close-button');
+const modalClose = document.querySelector('.modal__main_close');
+console.log(modalClose);
+const modalCloseButton = document.querySelector('.modal__close-button');
+console.log(modalCloseButton);
 
-const modalForm = document.querySelector('modal__form');
-const modalFormCheckbox = document.querySelector('form__input-checkbox');
-const modalFormDiscountInput = document.querySelector('form__input-discount');
+const modalForm = document.querySelector('.modal__form');
+console.log(modalForm);
+const modalFormCheckbox = document.querySelector('.form__input-checkbox');
+console.log(modalFormCheckbox);
+const modalFormDiscountInput = document.querySelector('.form__input-discount');
+console.log(modalFormDiscountInput);
 
-const modalTotalPriceNumber = document.querySelector('total__sum');
+const modalTotalPriceNumber = document.querySelector('.total__sum');
+console.log(modalTotalPriceNumber);
